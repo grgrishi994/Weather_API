f8eed3dd5bf2dae2f00807a5793d9df403e7080d
Rishika garg <grgrishi994@gmail.com>
2022-04-07 19:26:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Resturant from "./components/basics/Resturant";
+// import UseState from "./components/hooks/useState";
+// import UseEffect from "./components/hooks/useEffect";
+// import UseReducer from "./components/hooks/useReducer";
+import Temp from "./Components/weatherAPI/temp.js";
+const App = () => {
+  return (
+  <>
+  <Temp/>
+  </>
+  );
+  
+  };
+
+export default App;
